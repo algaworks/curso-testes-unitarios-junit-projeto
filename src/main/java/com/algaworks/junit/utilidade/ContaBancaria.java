e346278514a5030e60061dd78bb7a2233a21cdbf
Alex Alves <alexaugustoti@gmail.com>
2023-01-07 11:55:06 -0300

    Correções no desafio

--- src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
+++ src/main/java/com/algaworks/junit/utilidade/ContaBancaria.java
@@ -5,18 +5,18 @@ import java.math.BigDecimal;
 public class ContaBancaria {
 
     public ContaBancaria(BigDecimal saldo) {
-        //TODO 1 - validar saldo: não pode ser nulo, case seja deve lançar uma IllegalArgumentException
+        //TODO 1 - validar saldo: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
         //TODO 2 - pode ser zero ou negativo
     }
 
     public void saque(BigDecimal valor) {
-        //TODO 1 - validar valor: não pode ser nulo ou zero, case seja deve lançar uma IllegalArgumentException
+        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
         //TODO 2 - Deve subtrair o valor do saldo
         //TODO 3 - Se o saldo for insuficiente deve lançar uma RuntimeException
     }
 
     public void deposito(BigDecimal valor) {
-        //TODO 1 - validar valor: não pode ser nulo ou zero, case seja deve lançar uma IllegalArgumentException
+        //TODO 1 - validar valor: não pode ser nulo, zero ou menor que zero, caso seja, deve lançar uma IllegalArgumentException
         //TODO 2 - Deve adicionar o valor ao saldo
     }
 
