069dc96a5d01f2fd4436dd172efb9be819a5a707
Alex de Carvalho <leco_123carvalho@hotmail.com>
2023-11-24 17:53:55 -0300

    ajustar importes
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FiltroNumerosTest {
     
     @Test
