877c5e8e1506f7e90de066f20dba1cc1d34b4442
Alex de Carvalho <leco_123carvalho@hotmail.com>
2023-11-24 16:32:12 -0300

    2.7. Asserções agrupadas com AssertAll: [Exemplo lançando erro] -Se o primeiro método falhar para e o resto das outras asserções não serão executadas.

---
+++ src/test/java/com/algaworks/junit/utilidade/PessoaTest.java
@@ -0,0 +1,25 @@
+package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PessoaTest {
+
+    @Test
+    public void assercaoAgrupada() {
+        Pessoa pessoa = new Pessoa("Alex", "Carvalho");
+
+        assertAll(
+                "Asserções de pessoa",
+                ()-> assertEquals("Alex", pessoa.getNome()),
+                ()-> assertEquals("Silva", pessoa.getSobrenome()));
+
+        // assertAll faz a mesma coisa que o AssertEquals, porém a maior vantagem neste caso que ele executa todos
+        // para depois apresentar o erro de ambos, caso ambos estajam com erro, não parando na primeira asserção.
+
+        //assertEquals("Alex", pessoa.getNome());
+        //assertEquals("Silva", pessoa.getSobrenome());
+    }
+
+}
