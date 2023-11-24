d8c60e20f64900f4ec87bde9305e7c4c98dfadf2
Alex de Carvalho <leco_123carvalho@hotmail.com>
2023-11-24 17:32:25 -0300

    2.10. Ignorando execução dos testes condicionalmente com Assumptions existe duas formas: 1º) Criando variável de ambiente dentro do profile e adicionando o Assumptions.
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

--- src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SimuladorEsperaTest.java
@@ -1,18 +1,18 @@
 package com.algaworks.junit.utilidade;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class SimuladorEsperaTest {
 
     @Test
-    @Disabled("Não é mais aplicável")
+    //@Disabled("Não é mais aplicável")
     public void deveEsperarENaoDarTimeout() {
+        // Deve ser criado uma váriavel de ambiente no profile "ENV"="PROD"
+        Assumptions.assumeTrue("PROD".equals(System.getenv("ENV")), ()-> "Abortando teste: Não deve ser executado em Produção.");
 
         // Assegura que este método deveria ser executado em 1 segundo
         // Esse é o melhor método para validar o tempo de executação, porque não deixa bloqueado por 10 segundos
