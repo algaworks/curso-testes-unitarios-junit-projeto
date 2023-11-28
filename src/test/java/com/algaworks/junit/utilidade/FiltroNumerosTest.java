36ed50b62717f76a5f490080df49556db03db2d6
Alex de Carvalho <leco_123carvalho@hotmail.com>
2023-11-28 10:44:36 -0300

    3.1. Organizando testes com o padrão Triple A: apenas adicionando comentário para facilitar o entendimento do padrão `Triple A`.
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

--- src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
+++ src/test/java/com/algaworks/junit/utilidade/FiltroNumerosTest.java
@@ -11,10 +11,14 @@ class FiltroNumerosTest {
     @Test
     public void deveRetornarNumerosPares() {
 
+        // [A]rrange: Preparar as variáveis para o senário de teste;
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4);
         List<Integer> numerosParesEsperados = Arrays.asList(2, 4);
+
+        // [A]ctive: Executando o cenário;
         List<Integer> resultadoFiltro = FiltroNumeros.numerosPares(numeros);
 
+        // [A]ssert: Validar se o cenário está correto, com o resultado;
         //verificar tanto do conteúdo quanto da ordem
         Assertions.assertArrayEquals(numerosParesEsperados.toArray(new Object[]{}), resultadoFiltro.toArray(new Object[]{}));
 
