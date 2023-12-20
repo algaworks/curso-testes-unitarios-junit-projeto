134fef3ede2ee554507f9bea02151b114bc58aef
Alex de Carvalho <leco_123carvalho@hotmail.com>
2023-12-20 17:36:10 -0300

    4.7. Verificando chamada de métodos com mock usando Mockito verify
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -58,4 +58,14 @@ public class CadastroEditorComMockTest {
         assertEquals(1L, editorSalvo.getId());
     }
 
+    // Verificando chamada de métodos com mock usando Mockito verify
+    @Test
+    void Dado_um_editor_valido_Quando_criar_Entao_deve_chamar_metodo_salvar_do_armazenamento(){
+
+        cadastroEditor.criar(editor);
+        Mockito.verify(armazenamentoEditor, Mockito.times(1))
+                .salvar(Mockito.eq(editor));
+    }
+
+
 }
