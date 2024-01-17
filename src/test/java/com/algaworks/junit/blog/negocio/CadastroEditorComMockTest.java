39688a4d0f856fbb0bfe0f8d8938273e732f54cd
Alex de Carvalho <leco_123carvalho@hotmail.com>
2024-01-16 21:26:36 -0300

    4.11. Alterando retorno de um mock após chamadas consecutivas
    Signed-off-by: Alex de Carvalho <leco_123carvalho@hotmail.com>

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.junit.blog.negocio;
 
 import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
+import com.algaworks.junit.blog.exception.RegraNegocioException;
 import com.algaworks.junit.blog.modelo.Editor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -11,6 +12,7 @@ import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -95,6 +97,18 @@ public class CadastroEditorComMockTest {
         cadastroEditor.criar(editor);
         Mockito.verify(editor, Mockito.atLeast(1)).getEmail();
 
+    }
+
+    @Test
+    void Dado_um_editor_com_email_existente_Quando_cadastrar_Entao_deve_lancar_exception() {
+        Mockito.when(armazenamentoEditor.encontrarPorEmail("alex@email.com"))
+                .thenReturn(Optional.empty())
+                .thenReturn(Optional.of(editor));
+
+        Editor editorComEmailExistente = new Editor(null, "Alex", "alex@email.com", BigDecimal.TEN, true);
+
+        cadastroEditor.criar(editor);
+        assertThrows(RegraNegocioException.class, ()-> cadastroEditor.criar(editorComEmailExistente));
     }
 
 }
